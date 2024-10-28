a193560c6fff0c0c4f80422b4f1a57fe6e39340b
Andrew Taylor <andrew_t97@hotmail.co.uk>
2024-10-28 21:00:47 +0000

    Added TODO to check cap vector speed

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 
+// TODO: Validate this works in both negative and positive directions
 void cap_vector_to_max_speed(sf::Vector2f &vec, float maxSpeed) {
   float speed = std::sqrt(std::pow(vec.x, 2) + std::pow(vec.y, 2));
 
